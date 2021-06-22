5fa31c74b9b4f43b9ddda922c0f82cce31ec89f8
Anushree Das <ad1707@rit.edu>
2021-06-22 00:34:05 -0400

    Update generate.py
    Generate CPU usage logs and write to file

--- generate.py
+++ generate.py
@@ -4,27 +4,33 @@ import os
 from random import randrange
 import ipaddress
 
+def createDirectory(dirPath):
+    if not os.path.exists(dirPath):
+        os.makedirs(dirPath)
 
 def generateLogs(directory,date):
     timestamp = int(datetime.timestamp(date))
     # print(timestamp)
-    dirPath = os.path.join(directory,str(timestamp))
 
-    if not os.path.exists(dirPath):
-        print("Creating directory..")
-        os.makedirs(dirPath)
-
-    n = 1000
+    n = 2
     mask = 32-math.ceil(math.log(n, 2))
     # print(mask)
 
     ipAddresses = [str(ip) for ip in ipaddress.IPv4Network('192.168.0.0/'+str(mask))][:n]
 
-    # for _ in range(1440):
     for host_ip in ipAddresses:
+        server_path = os.path.join(directory,host_ip)
+        createDirectory(server_path)
         for cpu_id in range(1,3):
-            cpu_usage = randrange(0,100)
-            print(host_ip,cpu_id,cpu_usage)
+            cpu_path = os.path.join(server_path,str(cpu_id))
+            createDirectory(cpu_path)
+            date_file = os.path.join(cpu_path, str(timestamp)+'.csv')
+            # createDirectory(date_file)
+            with open(date_file,'w') as f:
+                for _ in range(1440):
+                    cpu_usage = randrange(0, 100)
+                    print(host_ip, cpu_id, cpu_usage)
+                    f.write(str(timestamp)+','+host_ip+','+str(cpu_id)+','+str(cpu_usage)+'\n')
 
 if __name__ == "__main__":
     today = datetime.now().now().replace(hour=0, minute=0, second=0, microsecond=0)
